c259fdd2bbc9f97f85158edcce07f28d0cb046a0
conelli BISKUPOVIC <conelliconi1200@gmail.com>
2026-01-17 17:37:47 +0300

    feat: ajout de src

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DataRetriever dataRetriever = new DataRetriever();
+        Dish dish = dataRetriever.findDishById(4
+
+        );
+        System.out.println(dish);
+
+        dish.setIngredients(List.of(new Ingredient(1), new Ingredient(2)));
+        Dish newDish = dataRetriever.saveDish(dish);
+        System.out.println(newDish);
+
+        List<Ingredient> createdIngredients = dataRetriever.createIngredients(List.of(new Ingredient(null, "Fromage", CategoryEnum.DAIRY, 1200.0)));
+        System.out.println(createdIngredients);
+    }
+}
