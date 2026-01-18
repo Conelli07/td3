9a90bbd089ae7d54cc8b5adbcdc4d5c226a2dfcb
conelli BISKUPOVIC <conelliconi1200@gmail.com>
2026-01-18 15:25:52 +0300

    feat: mise a jour des codes pour que ça marche avec la base

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -4,24 +4,18 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    public Connection getConnection() {
-        try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/mini_dish_db";
+    private static final String USER =
+            "mini_dish_db_manager";
+    private static final String PASSWORD =
+            "123456";
 
-    public void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur de connexion à la base", e);
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,27 @@
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
+            System.setErr(new java.io.PrintStream(System.err, true, StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         DataRetriever dataRetriever = new DataRetriever();
-        Dish dish = dataRetriever.findDishById(4
 
-        );
+        Dish dish = dataRetriever.findDishById(4);
         System.out.println(dish);
 
         dish.setIngredients(List.of(new Ingredient(1), new Ingredient(2)));
         Dish newDish = dataRetriever.saveDish(dish);
         System.out.println(newDish);
 
-        List<Ingredient> createdIngredients = dataRetriever.createIngredients(List.of(new Ingredient(null, "Fromage", CategoryEnum.DAIRY, 1200.0)));
+        List<Ingredient> createdIngredients = dataRetriever.createIngredients(
+                List.of(new Ingredient(null, "Fromage", CategoryEnum.DAIRY, 1200.0))
+        );
         System.out.println(createdIngredients);
     }
 }
